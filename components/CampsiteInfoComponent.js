84f829153cf39956163b4fc6f8878eb4017f4c8b
Caitlin Neathawk <neathawkcaitlin@gmail.com>
2021-12-21 12:50:59 -0800

    debug

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -10,7 +10,7 @@ const mapStateToProps = state => {
     return {
         campsites: state.campsites,
         comments: state.comments,
-        favorites: state.favories,
+        favorites: state.favorites,
     };
 };
 
